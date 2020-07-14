7e8464989a8bbb561063f7c88956c0a27882defa
rifat963 <rifat.abir@gmail.com>
2020-07-14 17:09:21 +0200

    change augment

--- train_config.py
+++ train_config.py
@@ -9,7 +9,7 @@ class TrainGlobalConfig:
     n_epochs = 100 # n_epochs = 40
     lr = 0.0002 # original 0.0002
 
-    folder = 'effdet5-filtered-data-cutmix-fold-4'
+    folder = 'effdet5-filtered-data-cutmix-fold-3'
 
     # -------------------
     verbose = True
