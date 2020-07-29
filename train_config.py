847bff85b95162f7bbc29ba1f1a212a32c4848ea
rifat963 <rifat.abir@gmail.com>
2020-07-29 19:34:42 +0200

    update

--- train_config.py
+++ train_config.py
@@ -7,7 +7,7 @@ class TrainGlobalConfig:
     num_workers = 16
     batch_size = 2
     n_epochs = 80 # n_epochs = 40
-    lr = 0.0002
+    lr = 0.001#0.0002
     mixed_precision = True
     accumulate = 16
     folder = 'effdet5-cutmix-mosaic-fold3-with-change-augmentation'
@@ -21,26 +21,26 @@ class TrainGlobalConfig:
     step_scheduler = False  # do scheduler.step after optimizer.step
     validation_scheduler = True  # do scheduler.step after validation stage loss
 
-    #     SchedulerClass = torch.optim.lr_scheduler.OneCycleLR
-    #     scheduler_params = dict(
-    #         max_lr=0.001,
-    #         epochs=n_epochs,
-    #         steps_per_epoch=int(len(train_dataset) / batch_size),
-    #         pct_start=0.1,
-    #         anneal_strategy='cos',
-    #         final_div_factor=10**5
-    #     )
-
-    SchedulerClass = torch.optim.lr_scheduler.ReduceLROnPlateau
+    SchedulerClass = torch.optim.lr_scheduler.OneCycleLR
     scheduler_params = dict(
-        mode='min',
-        factor=0.5,
-        patience=2,
-        verbose=False,
-        threshold=0.0001,
-        threshold_mode='abs',
-        cooldown=0,
-        min_lr=1e-8,
-        eps=1e-08
-    )
+             max_lr=0.001,
+             epochs=n_epochs,
+             steps_per_epoch=int(len(train_dataset) / batch_size),
+             pct_start=0.1,
+             anneal_strategy='cos',
+             final_div_factor=10**5
+         )
+
+    #SchedulerClass = torch.optim.lr_scheduler.ReduceLROnPlateau
+    #scheduler_params = dict(
+    #    mode='min',
+    #    factor=0.5,
+    #    patience=2,
+    #    verbose=False,
+    #    threshold=0.0001,
+    #    threshold_mode='abs',
+    #    cooldown=0,
+    #    min_lr=1e-8,
+    #    eps=1e-08
+    #)
     # --------------------
